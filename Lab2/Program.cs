98218dfccbc75468d705baeeb7edc163edf1fa2f
Asiyasalahadin <101065117+Asiyasalahadin@users.noreply.github.com>
2022-06-28 10:36:09 +0300

    added

---
+++ Lab2/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num;
+//ASSignment
+do
+{
+    Console.WriteLine("Enter a number");
+    num = Convert.ToInt32(Console.ReadLine());
+
+    if (num == 0)
+        Console.WriteLine("Neither odd nor even");
+    else if (num % 2 == 0)
+        Console.WriteLine("Number is Even");
+
+    else
+    {
+        Console.WriteLine("Number is Odd");
+    }
+
+} while (
